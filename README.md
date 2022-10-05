020778470e2852bd0b90c88189b7f59fc6e2b6f9
Yurino Oga <yurino.o@latona.io>
2022-10-05 21:35:52 +0900

    release

---
+++ README.md
@@ -0,0 +1,20 @@
+# data-platform-reservation-document-sql
+
+data-platform-reservation-document-sql は、データ連携基盤において、入出庫予定データを維持管理するSQLテーブルを作成するためのレポジトリです。 
+
+## 前提条件  
+data-platform-business-partner-sql は、データ連携にあたり、API を利用し、本レポジトリ の sql 設定ファイルの内容は、下記 URL の API 仕様を前提としています。
+https://api.XXXXXXXX.com/api/OP_API_XXXXXXX_XXX/overview  
+
+## sqlの設定ファイル
+
+data-platform-reservation-document-sql には、sqlの設定ファイルとして、以下のファイルが含まれています。  
+
+* data-platform-reservation-document-sql-header-data.sql（データ連携基盤 入出庫予定 - ヘッダデータ）
+* data-platform-reservation-document-sql-item-data.sql（データ連携基盤 入出庫予定 - 明細データ）
+
+## MySQLのセットアップ / Kubernetesの設定 / SQLテーブルの作成方法
+
+MySQLのセットアップ / Kubernetesの設定 / 具体的なSQLテーブルの作成方法、については、[mysql-kube](https://github.com/latonaio/mysql-kube)を参照ください。
+
+
---
+++ data-platform-reservation-document-sql-header-data.sql
@@ -0,0 +1,20 @@
+CREATE TABLE `data_platform_reservation_document_header_data`
+(
+    `BusinessPartner`       varchar(10) NOT NULL,  --新規追加
+    `Reservation`           varchar(10) NOT NULL,
+    `OrderID`               varchar(12) DEFAULT NULL,
+    `OrderItem`             varchar(10) DEFAULT NULL,  --新規追加
+    `GoodsMovementType`     varchar(3) DEFAULT NULL,
+    `GoodsRecipientName`    varchar(12) DEFAULT NULL,
+    `ReservationDate`       varchar(80) DEFAULT NULL,
+    `Customer`              varchar(10) DEFAULT NULL,
+    `Supplier`              varchar(10) DEFAULT NULL,
+    `Project`               varchar(24) DEFAULT NULL,
+    `OrderScheduleLine`     varchar(4) DEFAULT NULL,  --名称変更
+    `IssuingPlant`                  varchar(4) DEFAULT NULL,  --新規追加
+    `IssuingPlantStorageLocation`   varchar(4) DEFAULT NULL,  --新規追加
+    `ReceivingPlant`                varchar(4) DEFAULT NULL,  --新規追加
+    `ReceivingPlantStorageLocation` varchar(4) DEFAULT NULL,  --新規追加
+    PRIMARY KEY (`BusinessPartner`,`Reservation`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
