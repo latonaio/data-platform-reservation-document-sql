0d1bf6381f017422bdddd0454ebfd2a35e6b9ef1
MiyuYokoyama0109 <miyu.y@latona.io>
2022-10-17 21:34:57 +0900

    release

--- data-platform-reservation-document-sql-header-data.sql
+++ data-platform-reservation-document-sql-header-data.sql
@@ -1,18 +1,18 @@
 CREATE TABLE `data_platform_reservation_document_header_data`
 (
-    `BusinessPartner`               varchar(10) NOT NULL,      --新規追加
-    `Reservation`                   varchar(10) NOT NULL,
-    `OrderID`                       varchar(12) DEFAULT NULL,
-    `OrderItem`                     varchar(10) DEFAULT NULL,  --新規追加
+    `BusinessPartner`               int(10) NOT NULL,      --新規追加
+    `Reservation`                   int(10) NOT NULL,
+    `OrderID`                       int(10) DEFAULT NULL,
+    `OrderItem`                     int(10) DEFAULT NULL,  --新規追加
     `GoodsMovementType`             varchar(3) DEFAULT NULL,
-    `GoodsRecipientName`            varchar(12) DEFAULT NULL,
+    `GoodsRecipientName`            varchar(100) DEFAULT NULL,
     `ReservationDate`               varchar(80) DEFAULT NULL,
-    `Customer`                      varchar(10) DEFAULT NULL,
-    `Supplier`                      varchar(10) DEFAULT NULL,
+    `Customer`                      int(10) DEFAULT NULL,
+    `Supplier`                      int(10) DEFAULT NULL,
     `Project`                       varchar(24) DEFAULT NULL,
     `OrderScheduleLine`             varchar(4) DEFAULT NULL,  --名称変更
     `IssuingPlant`                  varchar(4) DEFAULT NULL,  --新規追加
-    `IssuingPlantStorageLocation`   varchar(4) DEFAULT NULL,  --新規追加
+    `IssuingPlantStorageLocation`   tinyint(1) DEFAULT NULL,  --新規追加
     `ReceivingPlant`                varchar(4) DEFAULT NULL,  --新規追加
     `ReceivingPlantStorageLocation` varchar(4) DEFAULT NULL,  --新規追加
     PRIMARY KEY (`BusinessPartner`, `Reservation`)
--- data-platform-reservation-document-sql-item-data.sql
+++ data-platform-reservation-document-sql-item-data.sql
@@ -1,13 +1,13 @@
-CREATE TABLE `data_platform_reservation_document_item_data`
+1CREATE TABLE `data_platform_reservation_document_item_data`
 (
-    `BusinessPartner`                   varchar(10) NOT NULL,
-    `Reservation`                       varchar(10) NOT NULL,
-    `ReservationItem`                   varchar(4) NOT NULL,
+    `BusinessPartner`                   int(10) NOT NULL,
+    `Reservation`                       int(10) NOT NULL,
+    `ReservationItem`                   int(4) NOT NULL,
     `RecordType`                        varchar(2) NOT NULL,
     `Product`                           varchar(40) DEFAULT NULL,
     `PDTCompRequirementDate`            varchar(80) DEFAULT NULL,  --名称変更
     `IssuingPlant`                      varchar(4) DEFAULT NULL,  --新規追加
-    `IssuingPlantStorageLocation`       varchar(4) DEFAULT NULL,  --新規追加
+    `IssuingPlantStorageLocation`       tinyint(1) DEFAULT NULL,  --新規追加
     `ReceivingPlant`                    varchar(4) DEFAULT NULL,  --新規追加
     `ReceivingPlantStorageLocation`     varchar(4) DEFAULT NULL,  --新規追加
     `Batch`                             varchar(10) DEFAULT NULL,
@@ -21,11 +21,11 @@ CREATE TABLE `data_platform_reservation_document_item_data`
     `ResvnItmRequiredQtyInEntryUnit`    varchar(13) DEFAULT NULL,
     `ResvnItmWithdrawnQtyInBaseUnit`    varchar(13) DEFAULT NULL,
     `ResvnItmWithdrawnAmtInBPCrcy`      varchar(11) DEFAULT NULL,  --名称変更
-    `GoodsRecipientName`                varchar(50) DEFAULT NULL,
+    `GoodsRecipientName`                varchar(100) DEFAULT NULL,
     `ReservationItemText`               varchar(100) DEFAULT NULL,
-    `ReferenceDocument`                 varchar(10) DEFAULT NULL,  --新規追加
-    `ReferenceDocumentItem`             varchar(6) DEFAULT NULL,   --新規追加
+    `ReferenceDocument`                 int(10) DEFAULT NULL,  --新規追加
+    `ReferenceDocumentItem`             int(6) DEFAULT NULL,   --新規追加
     PRIMARY KEY (`BusinessPartner`, `Reservation`, `ReservationItem`, `RecordType`),
-    CONSTRAINT `DataPlatformReservationDocumentItem_fk` FOREIGN KEY (`Reservation`) REFERENCES `data_platform_reservation_document_header_data` (`Reservation`)
+    CONSTRAINT `DataPlatformReservationDocumentItem_fk` FOREIGN KEY (`BusinessPartner`, `Reservation`) REFERENCES `data_platform_reservation_document_header_data` (`BusinessPartner`, `Reservation`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
