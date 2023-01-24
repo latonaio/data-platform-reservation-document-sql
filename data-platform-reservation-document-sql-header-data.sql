53e4aa241313f5298c59f5f7c773f0cfb811f3c2
MiyuYokoyama0109 <miyu.y@latona.io>
2023-01-24 16:24:48 +0900

    release

--- data-platform-reservation-document-sql-header-data.sql
+++ data-platform-reservation-document-sql-header-data.sql
@@ -1,20 +1,20 @@
 CREATE TABLE `data_platform_reservation_document_header_data`
 (
-    `BusinessPartner`               int(10) NOT NULL,      --新規追加
-    `Reservation`                   int(10) NOT NULL,
-    `OrderID`                       int(10) DEFAULT NULL,
-    `OrderItem`                     int(10) DEFAULT NULL,  --新規追加
+    `BusinessPartner`               int(12) NOT NULL,
+    `Reservation`                   int(16) NOT NULL,
+    `OrderID`                       int(16) DEFAULT NULL,
+    `OrderItem`                     int(6) DEFAULT NULL,
     `GoodsMovementType`             varchar(3) DEFAULT NULL,
     `GoodsRecipientName`            varchar(100) DEFAULT NULL,
     `ReservationDate`               varchar(80) DEFAULT NULL,
     `Customer`                      int(10) DEFAULT NULL,
     `Supplier`                      int(10) DEFAULT NULL,
     `Project`                       varchar(24) DEFAULT NULL,
-    `OrderScheduleLine`             varchar(4) DEFAULT NULL,  --名称変更
-    `IssuingPlant`                  varchar(4) DEFAULT NULL,  --新規追加
-    `IssuingPlantStorageLocation`   tinyint(1) DEFAULT NULL,  --新規追加
-    `ReceivingPlant`                varchar(4) DEFAULT NULL,  --新規追加
-    `ReceivingPlantStorageLocation` varchar(4) DEFAULT NULL,  --新規追加
+    `OrderScheduleLine`             varchar(4) DEFAULT NULL,
+    `IssuingPlant`                  varchar(4) DEFAULT NULL,
+    `IssuingPlantStorageLocation`   tinyint(1) DEFAULT NULL,
+    `ReceivingPlant`                varchar(4) DEFAULT NULL,
+    `ReceivingPlantStorageLocation` varchar(4) DEFAULT NULL,
     PRIMARY KEY (`BusinessPartner`, `Reservation`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
--- data-platform-reservation-document-sql-item-data.sql
+++ data-platform-reservation-document-sql-item-data.sql
@@ -1,30 +1,30 @@
 1CREATE TABLE `data_platform_reservation_document_item_data`
 (
-    `BusinessPartner`                   int(10) NOT NULL,
-    `Reservation`                       int(10) NOT NULL,
-    `ReservationItem`                   int(4) NOT NULL,
+    `BusinessPartner`                   int(12) NOT NULL,
+    `Reservation`                       int(16) NOT NULL,
+    `ReservationItem`                   int(6) NOT NULL,
     `RecordType`                        varchar(2) NOT NULL,
     `Product`                           varchar(40) DEFAULT NULL,
-    `PDTCompRequirementDate`            varchar(80) DEFAULT NULL,  --名称変更
-    `IssuingPlant`                      varchar(4) DEFAULT NULL,  --新規追加
-    `IssuingPlantStorageLocation`       tinyint(1) DEFAULT NULL,  --新規追加
-    `ReceivingPlant`                    varchar(4) DEFAULT NULL,  --新規追加
-    `ReceivingPlantStorageLocation`     varchar(4) DEFAULT NULL,  --新規追加
+    `PDTCompRequirementDate`            varchar(80) DEFAULT NULL,
+    `IssuingPlant`                      varchar(4) DEFAULT NULL,
+    `IssuingPlantStorageLocation`       tinyint(1) DEFAULT NULL,
+    `ReceivingPlant`                    varchar(4) DEFAULT NULL,
+    `ReceivingPlantStorageLocation`     varchar(4) DEFAULT NULL,
     `Batch`                             varchar(10) DEFAULT NULL,
     `BaseUnit`                          varchar(3) DEFAULT NULL,
     `GLAccount`                         varchar(10) DEFAULT NULL,
     `GoodsMovementType`                 varchar(3) DEFAULT NULL,
     `EntryUnit`                         varchar(3) DEFAULT NULL,
-    `BusinessPartnerCurrency`           varchar(5) DEFAULT NULL,  --新規追加
+    `BusinessPartnerCurrency`           varchar(5) DEFAULT NULL,
     `ResvnItmRequiredQtyInBaseUnit`     varchar(13) DEFAULT NULL,
     `ReservationItmIsMarkedForDeltn`    tinyint(1) DEFAULT NULL,
     `ResvnItmRequiredQtyInEntryUnit`    varchar(13) DEFAULT NULL,
     `ResvnItmWithdrawnQtyInBaseUnit`    varchar(13) DEFAULT NULL,
-    `ResvnItmWithdrawnAmtInBPCrcy`      varchar(11) DEFAULT NULL,  --名称変更
+    `ResvnItmWithdrawnAmtInBPCrcy`      varchar(11) DEFAULT NULL,
     `GoodsRecipientName`                varchar(100) DEFAULT NULL,
     `ReservationItemText`               varchar(100) DEFAULT NULL,
-    `ReferenceDocument`                 int(10) DEFAULT NULL,  --新規追加
-    `ReferenceDocumentItem`             int(6) DEFAULT NULL,   --新規追加
+    `ReferenceDocument`                 int(16) DEFAULT NULL,
+    `ReferenceDocumentItem`             int(6) DEFAULT NULL,
     PRIMARY KEY (`BusinessPartner`, `Reservation`, `ReservationItem`, `RecordType`),
     CONSTRAINT `DataPlatformReservationDocumentItem_fk` FOREIGN KEY (`BusinessPartner`, `Reservation`) REFERENCES `data_platform_reservation_document_header_data` (`BusinessPartner`, `Reservation`)
 ) ENGINE = InnoDB
